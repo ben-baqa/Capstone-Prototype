dc048c49e70a40d2e5d8bbbd76d6970f52cd77b9
Benjamin Nielsen <56326234+ben-baqa@users.noreply.github.com>
2021-12-23 13:54:48 -0700

    functional socket connection
    proof of concept for simple webSocket connection. Still lacking integration with full functionality.

--- src/components/channels/LandingPage.js
+++ src/components/channels/LandingPage.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Container, Button} from "react-bootstrap";
 import './LandingPage.css';
 
 export const LandingPage = () => {
+    const [socket, setSocket] = useState(null)
+    const [id, setID] = useState(-1)
+
+    useEffect(() => {
+        console.log('establishing socket connection')
+
+        let ws = new WebSocket('ws://localhost:3002')
+        ws.addEventListener('open', (e) => {
+            // ws.addEventListener('message', (e) => {
+            //     let msg = e.data
+            //     console.log('received from socket: ', msg)
+            //     if (msg.startsWith('connection id:'))
+            //         setID(parseInt(msg.split(':')[1]))
+            // })
+            // ws.send('new instance connected via socket')
+        })
+        ws.onmessage = (e) => {
+            let msg = e.data
+            console.log('received from socket: ', msg)
+            if (msg.startsWith('connection id:'))
+                setID(parseInt(msg.split(':')[1]))
+        }
+
+        window.onbeforeunload = () => {
+            ws.onclose = () => {}
+            ws.close()
+        }
+
+        setSocket(ws)
+    }, [])
+
     return(
         <Container>
             <div className="title-container">
@@ -10,6 +41,7 @@ export const LandingPage = () => {
             </div>
             <div className="link-container">
                 <Button id="link-button" href="/channels">Go To Channels</Button>
+                <Button onClick={()=> socket.send(`test:${id}`)}>Test Socket</Button>
             </div>
         </Container>
     )
