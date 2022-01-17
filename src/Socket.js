e2fc5a3557a5fcc1b82fb53866df80ea860de293
Benjamin Nielsen <56326234+ben-baqa@users.noreply.github.com>
2022-01-17 15:48:17 -0700

    Updated socket.js to connect to remote server + updated packages

--- src/Socket.js
+++ src/Socket.js
@@ -1,11 +1,19 @@
-// fetches a socket connection using the address defined in env.local
+// fetches a socket connection using the hardcoded address
 // This function returns a connected websocket, but lacks a connection id
 // The page following connection must wait for the id to be assigned before
 // replacing the onmessage listener
+
+// NOTE: if it refuses to establish a proper connection, you must
+// navigate to https://70.72.184.45:8080 , select advanced and allow
+// alternatively, https://localhost:8080 if you are working just on your local machine
 let getSocket = (setSocketID) => {
     console.log('establishing socket connection')
 
-    let ws = new WebSocket("wss://localhost:8080")
+    // for connecting to a test server on your local machine
+    // let ws = new WebSocket("wss://localhost:8080")
+    
+    // for connecting to the persistant development server
+    let ws = new WebSocket("wss://70.72.184.45:8080")
     ws.addEventListener('open', (e) => { 
         console.log("socket connection established")
     })
